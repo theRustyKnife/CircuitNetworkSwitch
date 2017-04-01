6e3ccc18143bb16497b0746f37f332d1bacbaac1
theRustyKnife <therustyknife13@gmail.com>
2017-04-01 20:20:57 +0200

    Refactor

--- circuit-network-switch/data.lua
+++ circuit-network-switch/data.lua
@@ -1,3 +1,7 @@
+local FML = require "FML.init"
+local config = require "config"
+
+
 local v_sprite = {
 	filename = "__circuit-network-switch__/graphics/switch/vertical-off.png",
 	width = 63,
@@ -5,7 +9,7 @@ local v_sprite = {
 	frame_count = 1,
 	shift = {0.15, 0},
 }
-local v_sprite_on = util.table.deepcopy(v_sprite)
+local v_sprite_on = FML.table.deep_copy(v_sprite)
 v_sprite_on.filename = "__circuit-network-switch__/graphics/switch/vertical-on.png"
 
 local h_sprite = {
@@ -16,409 +20,242 @@ local h_sprite = {
 	scale = 0.95,
 	shift = {0.2, -0.05},
 }
-local h_sprite_on = util.table.deepcopy(h_sprite)
+local h_sprite_on = FML.table.deep_copy(h_sprite)
 h_sprite_on.filename = "__circuit-network-switch__/graphics/switch/horizontal-on.png"
 
-data:extend({
-	--proxies for the wires to be switched
-	{
-		type = "constant-combinator",
-		name = "circuit-network-switch-proxy-trans",
+
+local trans_sprite = {
+	filename = "__circuit-network-switch__/graphics/trans.png",
+	x = 0,
+	y = 0,
+	width = 0,
+	height = 0,
+}
+local trans_sprites = {
+	north = trans_sprite,
+	east = trans_sprite,
+	south = trans_sprite,
+	west = trans_sprite,
+}
+
+
+-- the inner proxy combinator - the part that actually does the connecting
+FML.data.make_prototype{
+	base = FML.data.inherit("constant-combinator"),
+	properties = {
+		name = config.PROXY_IN_NAME,
 		icon = "__circuit-network-switch__/graphics/trans.png",
-		flags = {"placeable-neutral", "player-creation"},
 		selectable_in_game = false,
-		max_health = 100000,
-		order = "itsdafirstman",
-		
-		collision_box = {{-0.35, -0.35}, {0.35, 0.35}},
+		collision_box = {{-0.1, -0.1}, {0.1, 0.1}}, -- can't be 0 because of weird alignment issues
 		collision_mask = {},
-		
 		item_slot_count = 0,
-
-		sprites =
-		{
-			north =
+		
+		circuit_wire_connection_points = {
 			{
-				filename = "__circuit-network-switch__/graphics/trans.png",
-				x = 0,
-				y = 0,
-				width = 0,
-				height = 0,
-				frame_count = 1,
-				shift = {0.140625, 0.140625},
-			},
-			east =
-			{
-				filename = "__circuit-network-switch__/graphics/trans.png",
-				y = 0,
-				width = 0,
-				height = 0,
-				frame_count = 1,
-				shift = {0.140625, 0.140625},
-			},
-			south =
-			{
-				filename = "__circuit-network-switch__/graphics/trans.png",
-				x = 0,
-				y = 0,
-				width = 0,
-				height = 0,
-				frame_count = 1,
-				shift = {0.140625, 0.140625},
-			},
-			west =
-			{
-				filename = "__circuit-network-switch__/graphics/trans.png",
-				y = 0,
-				width = 0,
-				height = 0,
-				frame_count = 1,
-				shift = {0.140625, 0.140625},
-			}
-		},
-
-		activity_led_sprites =
-		{
-			north =
-			{
-				filename = "__circuit-network-switch__/graphics/trans.png",
-				width = 0,
-				height = 0,
-				frame_count = 1,
-				shift = {0.296875, -0.40625},
-			},
-			east =
-			{
-				filename = "__circuit-network-switch__/graphics/trans.png",
-				width = 0,
-				height = 0,
-				frame_count = 1,
-				shift = {0.25, -0.03125},
-			},
-			south =
-			{
-				filename = "__circuit-network-switch__/graphics/trans.png",
-				width = 0,
-				height = 0,
-				frame_count = 1,
-				shift = {-0.296875, -0.078125},
-			},
-			west =
-			{
-				filename = "__circuit-network-switch__/graphics/trans.png",
-				width = 0,
-				height = 0,
-				frame_count = 1,
-				shift = {-0.21875, -0.46875},
-			}
-		},
-
-		activity_led_light =
-		{
-			intensity = 0,
-			size = 0,
-		},
-
-		activity_led_light_offsets =
-		{
-			{0.296875, -0.40625},
-			{0.25, -0.03125},
-			{-0.296875, -0.078125},
-			{-0.21875, -0.46875}
-		},
-
-		circuit_wire_connection_points =
-		{
-			{
-				shadow =
-				{
+				shadow = {
 					red = {0.21875, -0.1},
-					green = {0.75, -0.1}
+					green = {0.75, -0.1},
 				},
-				wire =
-				{
+				wire = {
 					red = {-0.35, -0.1},
-					green = {0.35, -0.1}
-				}
+					green = {0.35, -0.1},
+				},
 			},
 			{
-				shadow =
-				{
+				shadow = {
 					red = {0.15, -0.15625},
 					green = {0.15, 0.25},
 				},
-				wire =
-				{
+				wire = {
 					red = {-0.13125, -0.5},
 					green = {-0.13125, -0.09375},
-				}
-			},
-			{
-				shadow =
-				{
-					red = {0.75, 0.5625},
-					green = {0.21875, 0.5625}
 				},
-				wire =
-				{
-					red = {0.35, 0.15625},
-					green = {-0.35, 0.15625}
-				}
 			},
 			{
-				shadow =
-				{
+				shadow = {
+					red = {0.75, 0.5625},
+					green = {0.21875, 0.5625},
+				},
+				wire = {
+					red = {0.35, 0.15625},
+					green = {-0.35, 0.15625},
+				},
+			},
+			{
+				shadow = {
 					red = {0.56875, 0.28125},
 					green = {0.56875, -0.125},
 				},
-				wire =
-				{
+				wire = {
 					red = {0.13125, -0.05},
 					green = {0.13125, -0.50625},
-				}
-			}
+				},
+			},
 		},
 		
 		circuit_wire_max_distance = 3,
-	},
-	{
-		type = "constant-combinator",
-		name = "circuit-network-switch-proxy",
-		icon = "__circuit-network-switch__/graphics/trans.png",
-		flags = {"placeable-neutral", "player-creation"},
-		max_health = 100000,
-		order = "itsdafirstman",
 		
-		collision_box = {{-0.35, -0.35}, {0.35, 0.35}},
-		collision_mask = {},
-		selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
-
-		item_slot_count = 0,
-
-		sprites =
-		{
-			north =
-			{
-				filename = "__circuit-network-switch__/graphics/trans.png",
-				x = 0,
-				y = 0,
-				width = 0,
-				height = 0,
-				frame_count = 1,
-				shift = {0.140625, 0.140625},
-			},
-			east =
-			{
-				filename = "__circuit-network-switch__/graphics/trans.png",
-				y = 0,
-				width = 0,
-				height = 0,
-				frame_count = 1,
-				shift = {0.140625, 0.140625},
-			},
-			south =
-			{
-				filename = "__circuit-network-switch__/graphics/trans.png",
-				x = 0,
-				y = 0,
-				width = 0,
-				height = 0,
-				frame_count = 1,
-				shift = {0.140625, 0.140625},
-			},
-			west =
-			{
-				filename = "__circuit-network-switch__/graphics/trans.png",
-				x = 0,
-				y = 0,
-				width = 0,
-				height = 0,
-				frame_count = 1,
-				shift = {0.140625, 0.140625},
-			}
-		},
-
-		activity_led_sprites =
-		{
-			north =
-			{
-				filename = "__circuit-network-switch__/graphics/trans.png",
-				width = 0,
-				height = 0,
-				frame_count = 1,
-				shift = {0.296875, -0.40625},
-			},
-			east =
-			{
-				filename = "__circuit-network-switch__/graphics/trans.png",
-				width = 0,
-				height = 0,
-				frame_count = 1,
-				shift = {0.25, -0.03125},
-			},
-			south =
-			{
-				filename = "__circuit-network-switch__/graphics/trans.png",
-				width = 0,
-				height = 0,
-				frame_count = 1,
-				shift = {-0.296875, -0.078125},
-			},
-			west =
-			{
-				filename = "__circuit-network-switch__/graphics/trans.png",
-				width = 0,
-				height = 0,
-				frame_count = 1,
-				shift = {-0.21875, -0.46875},
-			}
-		},
-
-		activity_led_light =
-		{
+		sprites = trans_sprites,
+		activity_led_sprites = trans_sprites,
+		
+		activity_led_light = {
 			intensity = 0,
 			size = 0,
 		},
+		activity_led_light_offsets = {{0, 0}, {0, 0}, {0, 0}, {0, 0}},
+	},
+	auto_generate = {"item"},
+}
 
-		activity_led_light_offsets =
-		{
-			{0.296875, -0.40625},
-			{0.25, -0.03125},
-			{-0.296875, -0.078125},
-			{-0.21875, -0.46875}
-		},
-
-		circuit_wire_connection_points =
-		{
+-- the outer proxy - selectable, only for user interaction
+FML.data.make_prototype{
+	base = FML.data.inherit("constant-combinator", config.PROXY_IN_NAME),
+	properties = {
+		name = config.PROXY_OUT_NAME,
+		selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
+		selectable_in_game = true,
+		
+		circuit_wire_connection_points = {
 			{
-				shadow =
-				{
+				shadow = {
 					red = {0.25, 0},
-					green = {0.75, 0}
+					green = {0.75, 0},
 				},
-				wire =
-				{
+				wire = {
 					red = {-0.25, -0.1},
 					green = {0.28125, -0.1},
-				}
+				},
 			},
 			{
-				shadow =
-				{
+				shadow = {
 					red = {0.15, -0.15625},
 					green = {0.15, 0.25},
 				},
-				wire =
-				{
+				wire = {
 					red = {-0.13125, -0.5},
 					green = {-0.13125, -0.09375},
-				}
-			},
-			{
-				shadow =
-				{
-					red = {0.25, 0.2},
-					green = {0.75, 0.2}
 				},
-				wire =
-				{
-					red = {-0.25, -0.15},
-					green = {0.28125, -0.15}
-				}
 			},
 			{
-				shadow =
-				{
+				shadow = {
+					red = {0.25, 0.2},
+					green = {0.75, 0.2},
+				},
+				wire = {
+					red = {-0.25, -0.15},
+					green = {0.28125, -0.15},
+				},
+			},
+			{
+				shadow = {
 					red = {0.56875, 0.28125},
 					green = {0.56875, -0.125},
 				},
-				wire =
-				{
+				wire = {
 					red = {0.13125, -0.05},
 					green = {0.13125, -0.50625},
-				}
+				},
 			}
 		},
 
-		circuit_wire_max_distance = 7.5
+		circuit_wire_max_distance = 7.5,
 	},
-	--the 'middle piece' that's going to control the thing
-	{
-		type = "lamp",
-		name = "circuit-network-switch",
-		order = "orderman",
-		icon = "__circuit-network-switch__/graphics/icons/switch.png",
-		flags = {"placeable-neutral", "player-creation"},
-		minable = {hardness = 0.2, mining_time = 0.5, result = "circuit-network-switch"},
+	auto_generate = {"item"},
+}
+
+
+-- the actual switch entity - horizontal position
+FML.data.make_prototype{
+	base = FML.data.inherit("lamp", "small-lamp"),
+	properties = {
+		name = config.SWITCH_H_NAME,
 		max_health = 55,
-		corpse = "small-remnants",
+		icon = "__circuit-network-switch__/graphics/icons/switch.png",
+		
 		collision_box = {{-1.15, -0.15}, {1.15, 0.15}},
 		selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
-		vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
-		energy_source =
-		{
-			type = "electric",
-			usage_priority = "secondary-input"
-		},
+		
 		energy_usage_per_tick = "1KW",
 		
 		picture_off = h_sprite,
 		picture_on = h_sprite_on,
-
-		circuit_wire_connection_point =
-		{
-			shadow =
-			{
+		
+		circuit_wire_connection_point = {
+			shadow = {
 				red = {0.734375, 0.578125},
 				green = {0.609375, 0.640625},
 			},
-			wire =
-			{
+			wire = {
 				red = {-0.25, -0.65},
 				green = {0.25, -0.65},
-			}
+			},
 		},
-		circuit_wire_max_distance = 7.5
+		circuit_wire_max_distance = 7.5,
+	},
+	auto_generate = {"item"},
+}.minable.result = config.ITEM_NAME
+
+-- the switch entity vertical version (lamps can't be rotated)
+FML.data.make_prototype{
+	base = FML.data.inherit("lamp", config.SWITCH_H_NAME),
+	properties = {
+		name = config.SWITCH_V_NAME,
+		collision_box = {{-0.15, -1.15}, {0.15, 1.15}},
+		picture_off = v_sprite,
+		picture_on = v_sprite_on,
+		
+		circuit_wire_connection_point = {
+			shadow = {
+				red = {0.734375, 0.578125},
+				green = {0.609375, 0.640625},
+			},
+			wire = {
+				red = {-0.25, -0.4},
+				green = {0.25, -0.4},
+			},
+		},
+	},
+	auto_generate = {"item"},
+}.minable.result = config.ITEM_NAME
+
+FML.data.make_prototypes{
+	-- thing that's going to be placed - can't be direct because lamps are not rotatable
+	{
+		base = FML.data.inherit("constant-combinator"),
+		properties = {
+			name = config.SWITCH_NAME,
+			sprites = {
+				north = v_sprite,
+				east = h_sprite,
+				south = v_sprite,
+				west = h_sprite
+			},
+			collision_box = data.raw["lamp"][config.SWITCH_V_NAME].collision_box,
+		},
 	},
 	{
 		type = "item",
-		name = "circuit-network-switch",
+		name = config.ITEM_NAME,
 		icon = "__circuit-network-switch__/graphics/icons/switch.png",
 		flags = {"goes-to-quickbar"},
 		subgroup = "circuit-network",
-		order = "a[robot]-b[construction-robot]",
-		place_result = "circuit-network-switch-place-proxy",
-		stack_size = 50
+		order = "b[combinators]-f[circuit-network-switch]",
+		place_result = config.SWITCH_NAME,
+		stack_size = 50,
 	},
 	{
 		type = "recipe",
-		name = "circuit-network-switch",
-		enabled = "false",
-		ingredients =
-		{
+		name = config.ITEM_NAME,
+		enabled = false,
+		ingredients = {
 			{"copper-cable", 5},
 			{"electronic-circuit", 5},
 		},
-		result = "circuit-network-switch"
+		result = config.ITEM_NAME,
 	},
-})
-
-local switch_v = util.table.deepcopy(data.raw["lamp"]["circuit-network-switch"])
-switch_v.name = switch_v.name .. "-v"
-switch_v.collision_box = {{-0.15, -1.15}, {0.15, 1.15}}
-switch_v.picture_off = v_sprite
-switch_v.picture_on = v_sprite_on
-switch_v.circuit_wire_connection_point = {
-	shadow =
-	{
-		red = {0.734375, 0.578125},
-		green = {0.609375, 0.640625},
-	},
-	wire =
-	{
-		red = {-0.25, -0.4},
-		green = {0.25, -0.4},
-	}
 }
 
-data:extend{switch_v}
+table.insert(data.raw["technology"]["circuit-network"].effects, {
+		type = "unlock-recipe",
+		recipe = config.ITEM_NAME,
+	})
